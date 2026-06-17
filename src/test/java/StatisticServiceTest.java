9d5f0d57634fbca4781248f2ad21da93b4d01712
Kate Shpakovskaya <kate.shpakovskaya@icloud.com>
2026-06-17 20:47:02 +0300

    New commit

--- src/test/java/StatisticServiceTest.java
+++ src/test/java/StatisticServiceTest.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class StatisticServiceTest {
+public class
+StatisticServiceTest {
 
     @Test
      public void findMax() {
@@ -13,5 +14,17 @@ public class StatisticServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void findIfMax() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 15};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+
     }
 }
